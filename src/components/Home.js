922d63ac38c5bf9b84c7c816b1b8664b91a49f13
kambleaa007 <ashishkamble191191@gmail.com>
2021-08-18 19:20:43 +0530

    ...

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,33 +30,44 @@ function HomeComponent(props) {
                     <div className="home-body-heading">Featured Cat</div>                    
                 </Row>
                 <Row>
-                <div>
+                <div className="home-body">
                     <CardGroup>
                         <Card>
-                            <Card.Img src={Cat} height="100%" width="100%"/>
+                            <Card.Img className="d-block mx-auto img-fluid w-50" src={Cat} height="100px" width="100%"/>
                             <Card.Body>
                             <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                                <p>
+                                    Name: Jane Doe <br/>
+                                    Breed:  Tabby <br/>
+                                    Age: 2 <br/>
+                                    Special Needs: easily scared, missing one front paw
+                                </p>                                                         
                             </Card.Text>
                             </Card.Body>
                         </Card>
                         <Card>
-                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Img className="d-block mx-auto img-fluid w-50" src={Cat} height="100px" width="100%"/>
                             <Card.Body>
                             <Card.Text>
-                                This card has supporting text below as a natural lead-in to additional
-                                content.{' '}
+                                <p>
+                                    Name: Jane Doe <br/>
+                                    Breed:  Tabby <br/>
+                                    Age: 2 <br/>
+                                    Special Needs: easily scared, missing one front paw
+                                </p>
                             </Card.Text>
                             </Card.Body>
                         </Card>
                         <Card>
-                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Img className="d-block mx-auto img-fluid w-50" src={Cat} height="100px" width="100%"/>
                             <Card.Body>
                             <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This card has even longer content than the first to
-                                show that equal height action.
+                                <p>
+                                    Name: Jane Doe <br/>
+                                    Breed:  Tabby <br/>
+                                    Age: 2 <br/>
+                                    Special Needs: easily scared, missing one front paw
+                                </p>
                             </Card.Text>
                             </Card.Body>
                         </Card>
