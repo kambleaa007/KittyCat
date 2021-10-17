18fb61faa09445c6c8ceb0608e326aad551ccba4
Rahul Sarkar <thecoderrahul@gmail.com>
2021-10-17 14:48:51 +0530

    Added typing effect, added hover effect on cards, fixed footer, fixed margin and padding

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -7,12 +7,12 @@ function Footer() {
 
     return(
       <div>
-        <Container>
+        <Container style={{ backgroundColor: "#f1f1f1"  }}>
           <Row>
-            <Col sm={1}>
+            <Col sm={1}  className="my-3">
               <img src={BigLogo} width="100px" height="auto" />
             </Col>
-            <Col sm={4}>
+            <Col sm={4} className="my-3">
               <div className="footer-top-text">Tine spent with cats is never wasted</div>
               <div className="footer-bottom-text">
                 Mailing Address: 111 Foster Place, Toronto ON XXX XXX
@@ -21,16 +21,21 @@ function Footer() {
                 Canadian Registered Charity number:  00000 0000 RR0001
             </div>
             </Col>
-            <Col sm={5}>
-              <div className="footer-button">
+            <Col sm={5} className="my-3">
+              <div className="footer-button" style={{ cursor:"pointer" }}>
                 <Row>Home</Row>
                 <Row>Adopt a Cat</Row>
                 <Row>About Us</Row>
                 <Row>Faq</Row>
               </div>
             </Col>
-            <Col sm={2}>
-              social
+            <Col sm={2} className="my-3">
+              <div className="footer-button" style={{ cursor:"pointer" }}>
+                <Row>Facebook</Row>
+                <Row>Instagram</Row>
+                <Row>Twitter</Row>
+                <Row>Youtube</Row>
+              </div>
             </Col>
           </Row>
         </Container>
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -7,14 +7,14 @@ import Map from "./../common/img/map.jpg";
 
 function ContactUs(props) {
     return (
-        <div className="about-us">
+        <div className="about-us mt-5">
             <Container>
                 <div>
                 <Row>
                     <Col sm={6}>
-                        <div className="about-us-text">
+                        <div className="about-us-text m-2">
                             <div className="about-us-text-heading">Contact Us</div>
-                            <div className="contact-us-text-body">
+                            <div className="contact-us-text-body m-3 ">
                                 <Row>Name: <input type="text" /></Row>
                                 <Row>Email: <input type="text" /></Row>
                                 <Row>Phone Number: <input type="text" /></Row>
@@ -24,7 +24,7 @@ function ContactUs(props) {
                         </div>
                     </Col>
                     <Col sm={6}>
-                        <div className="contact-us-map">
+                        <div className="contact-us-map m-3">
                             <img src={Map} />
                         </div>
                     </Col>
