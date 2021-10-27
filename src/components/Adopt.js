72c57960e656933026a509098130fa1598e0524a
irsayvid <dsdarimisetti@gmail.com>
2021-10-27 13:25:13 +0530

    add modal for learn more

--- src/components/Adopt.js
+++ src/components/Adopt.js
@@ -1,100 +1,173 @@
-import React from 'react'
-import { Container, Navbar, Nav, Col, Row, Table } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Container, Row, Table } from 'react-bootstrap'
+import CatModal from './Modal'
 
-
-import './../App.css';
-import Cat from "./../common/img/cat_headshot.jpg";
+import './../App.css'
+import Cat from './../common/img/cat_headshot.jpg'
 
 function Adopt(props) {
-    return (
-        <div className="">
-            <Container>
-                <div>
-                    <div className="adoption-heading">Adoption List</div>
-                    <div className="adoption-body">
-                        <Table striped bordered hover className="table">
-                            <thead className="table-heading">
-                                <tr>
-                                <th></th>
-                                <th>Name</th>
-                                <th>Breed</th>
-                                <th>Age</th>
-                                <th>More Info</th>
-                                <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                <td><img src={Cat} /></td>
-                                <td>Whiskers</td>
-                                <td>American ShortHair</td>
-                                <td>2 yrs Old</td>
-                                <td><div className="learn-more"><button>Learn More</button></div></td>
-                                <td>
-                                    <div className="adopt-me">
-                                        <Row>
-                                        <button>Adopt Me</button>
-                                        <i className="fa fa-edit" style={{fontSize:20,color:"#FC46AA"}}></i> 
-                                        <i className="fa fa-trash" style={{fontSize:20,color:"#FC46AA"}}></i>
-                                        </Row>
-                                    </div>
-                                </td>
-                                </tr>
-                                <tr>
-                                <td><img src={Cat} /></td>
-                                <td>Whiskers</td>
-                                <td>American ShortHair</td>
-                                <td>2 yrs Old</td>
-                                <td><div className="learn-more"><button>Learn More</button></div></td>
-                                <td>
-                                    <div className="adopt-me">
-                                        <Row>
-                                        <button>Adopt Me</button>
-                                        <i className="fa fa-edit" style={{fontSize:20,color:"#FC46AA"}}></i> 
-                                        <i className="fa fa-trash" style={{fontSize:20,color:"#FC46AA"}}></i>
-                                        </Row>
-                                    </div>
-                                </td>
-                                </tr>
-                                <tr>
-                                <td><img src={Cat} /></td>
-                                <td>Whiskers</td>
-                                <td>American ShortHair</td>
-                                <td>2 yrs Old</td>
-                                <td><div className="learn-more"><button>Learn More</button></div></td>
-                                <td>
-                                    <div className="adopt-me">
-                                        <Row>
-                                        <button>Adopt Me</button>
-                                        <i className="fa fa-edit" style={{fontSize:20,color:"#FC46AA"}}></i> 
-                                        <i className="fa fa-trash" style={{fontSize:20,color:"#FC46AA"}}></i>
-                                        </Row>
-                                    </div>
-                                </td>
-                                </tr>
-                                <tr>
-                                <td><img src={Cat} /></td>
-                                <td>Whiskers</td>
-                                <td>American ShortHair</td>
-                                <td>2 yrs Old</td>
-                                <td><div className="learn-more"><button>Learn More</button></div></td>
-                                <td>
-                                    <div className="adopt-me">
-                                        <Row>
-                                        <button>Adopt Me</button>
-                                        <i className="fa fa-edit" style={{fontSize:20,color:"#FC46AA"}}></i> 
-                                        <i className="fa fa-trash" style={{fontSize:20,color:"#FC46AA"}}></i>
-                                        </Row>
-                                    </div>
-                                </td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </div>
-                </div>
-            </Container>
-        </div>
-    );
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+  return (
+    <>
+      <div className="">
+        <Container>
+          <div>
+            <div className="adoption-heading">Adoption List</div>
+            <div className="adoption-body">
+              <Table striped bordered hover className="table">
+                <thead className="table-heading">
+                  <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Breed</th>
+                    <th>Age</th>
+                    <th>More Info</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <img src={Cat} />
+                    </td>
+                    <td>Whiskers</td>
+                    <td>American ShortHair</td>
+                    <td>2 yrs Old</td>
+                    <td>
+                      <div className="learn-more">
+                        <button onClick={handleShow}>Learn More</button>
+                        <CatModal
+                          show={show}
+                          onHide={handleClose}
+                          handleClose={handleClose}
+                        />
+                      </div>
+                    </td>
+                    <td>
+                      <div className="adopt-me">
+                        <Row>
+                          <button>Adopt Me</button>
+                          <i
+                            className="fa fa-edit"
+                            style={{ fontSize: 20, color: '#FC46AA' }}
+                          ></i>
+                          <i
+                            className="fa fa-trash"
+                            style={{ fontSize: 20, color: '#FC46AA' }}
+                          ></i>
+                        </Row>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <img src={Cat} />
+                    </td>
+                    <td>Whiskers</td>
+                    <td>American ShortHair</td>
+                    <td>2 yrs Old</td>
+                    <td>
+                      <div className="learn-more">
+                        <button onClick={handleShow}>Learn More</button>
+                        <CatModal
+                          show={show}
+                          onHide={handleClose}
+                          handleClose={handleClose}
+                        />
+                      </div>
+                    </td>
+                    <td>
+                      <div className="adopt-me">
+                        <Row>
+                          <button>Adopt Me</button>
+                          <i
+                            className="fa fa-edit"
+                            style={{ fontSize: 20, color: '#FC46AA' }}
+                          ></i>
+                          <i
+                            className="fa fa-trash"
+                            style={{ fontSize: 20, color: '#FC46AA' }}
+                          ></i>
+                        </Row>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <img src={Cat} />
+                    </td>
+                    <td>Whiskers</td>
+                    <td>American ShortHair</td>
+                    <td>2 yrs Old</td>
+                    <td>
+                      <div className="learn-more">
+                        <button onClick={handleShow}>Learn More</button>
+                        <CatModal
+                          show={show}
+                          onHide={handleClose}
+                          handleClose={handleClose}
+                        />
+                      </div>
+                    </td>
+                    <td>
+                      <div className="adopt-me">
+                        <Row>
+                          <button>Adopt Me</button>
+                          <i
+                            className="fa fa-edit"
+                            style={{ fontSize: 20, color: '#FC46AA' }}
+                          ></i>
+                          <i
+                            className="fa fa-trash"
+                            style={{ fontSize: 20, color: '#FC46AA' }}
+                          ></i>
+                        </Row>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <img src={Cat} />
+                    </td>
+                    <td>Whiskers</td>
+                    <td>American ShortHair</td>
+                    <td>2 yrs Old</td>
+                    <td>
+                      <div className="learn-more">
+                        <button onClick={handleShow}>Learn More</button>
+                        <CatModal
+                          show={show}
+                          onHide={handleClose}
+                          handleClose={handleClose}
+                        />
+                      </div>
+                    </td>
+                    <td>
+                      <div className="adopt-me">
+                        <Row>
+                          <button>Adopt Me</button>
+                          <i
+                            className="fa fa-edit"
+                            style={{ fontSize: 20, color: '#FC46AA' }}
+                          ></i>
+                          <i
+                            className="fa fa-trash"
+                            style={{ fontSize: 20, color: '#FC46AA' }}
+                          ></i>
+                        </Row>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </>
+  )
 }
 
-export default Adopt;
+export default Adopt
