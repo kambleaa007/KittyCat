e20d70be90b1fb80175a020ec2ca877bd6120c2a
priyanshuSharma-WebDev <priyanshushrama709@gmail.com>
2021-10-30 01:26:38 +0530

    improve style in footer

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -2,45 +2,49 @@ import { Container, Navbar, Nav, Col, Row } from 'react-bootstrap';
 
 import './../App.css';
 import BigLogo from "./img/BigLogo.png";
+import Facebook from "./img/facebook.png"
+import Insta from "./img/insta.png"
+import Twitter from "./img/twitter.png"
+import Youtube from "./img/youtube.png"
+
+
+
 
 function Footer() {
 
-    return(
-      <div>
-        <Container style={{ backgroundColor: "#f1f1f1"  }}>
-          <Row>
-            <Col sm={1}  className="my-3">
-              <img src={BigLogo} width="100px" height="auto" />
-            </Col>
-            <Col sm={4} className="my-3">
-              <div className="footer-top-text">Tine spent with cats is never wasted</div>
-              <div className="footer-bottom-text">
-                Mailing Address: 111 Foster Place, Toronto ON XXX XXX
-                email: adopt@kittycatadoptions.ca
-                Phone: 111-222-3333
-                Canadian Registered Charity number:  00000 0000 RR0001
-            </div>
-            </Col>
-            <Col sm={5} className="my-3">
-              <div className="footer-button" style={{ cursor:"pointer" }}>
-                <Row>Home</Row>
-                <Row>Adopt a Cat</Row>
-                <Row>About Us</Row>
-                <Row>Faq</Row>
-              </div>
-            </Col>
-            <Col sm={2} className="my-3">
-              <div className="footer-button" style={{ cursor:"pointer" }}>
-                <Row>Facebook</Row>
-                <Row>Instagram</Row>
-                <Row>Twitter</Row>
-                <Row>Youtube</Row>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    )
-  }
+  return (
+    <div className="footer-width">
 
-  export default Footer;  
+      <Row>
+          <Col sm={1} className="my-3">
+            <img src={BigLogo} width="100px" height="auto" />
+          </Col>
+          <Col sm={4} className="my-3">
+            <div className="footer-top-text">Time spent with cats is never wasted</div>
+            <div className="footer-bottom-text">
+              Mailing Address: 111 Foster Place, Toronto ON XXX XXX
+              email: adopt@kittycatadoptions.ca
+              Phone: 111-222-3333
+              Canadian Registered Charity number:  00000 0000 RR0001
+            </div>
+          </Col>
+        <Row style={{ width: "300px", display: "flex", alignItems: "center", justifyContent: "space-between", margin: "0 20px" }} sm={5} className="my-3">
+          <Col style={{ all: "initial", fontFamily: "Montserrat", fontSize: "0.8rem", color: "#fff" }}>Home</Col>
+          <Col style={{ all: "initial", fontFamily: "Montserrat", fontSize: "0.8rem", color: "#fff" }}>Adopt a Cat</Col>
+          <Col style={{ all: "initial", fontFamily: "Montserrat", fontSize: "0.8rem", color: "#fff" }}>About Us</Col>
+          <Col style={{ all: "initial", fontFamily: "Montserrat", fontSize: "0.8rem", color: "#fff" }}>Faq</Col>
+        </Row>
+        <Col sm={2} className="my-3">
+          <div className="footer-button" style={{ cursor: "pointer" }}>
+            <Col><img src={Facebook} /></Col>
+            <Col><img src={Insta} /></Col>
+            <Col><img src={Twitter} /></Col>
+            <Col><img src={Youtube} /></Col>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default Footer;
